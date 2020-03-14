8cc57e474b30fe78e73715adc9b2de20099fab31
wikk2207 <wiktoriajuliasalamon@gmail.com>
2020-03-15 00:32:15 +0100

    fix(ci-cd): add jest configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello World");
-})
+app.get('/', (request, response) => {
+	response.send('Hello World');
+});
 
 module.exports = app;
