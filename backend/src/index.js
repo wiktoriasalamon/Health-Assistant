6d36ed46ac2ec302da6a834c46d84c7140772210
wikk2207 <wiktoriajuliasalamon@gmail.com>
2020-03-14 20:56:19 +0100

    build(project): buld project structure

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const app = require('./app');
+const logger = require('./config/logger');
+require('dotenv').config()
+
+let server;
+server = app.listen(process.env.APP_PORT, () => {
+    logger.info(`Listening to port ${process.env.APP_PORT}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = error => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
